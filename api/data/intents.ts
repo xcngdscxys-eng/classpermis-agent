552ac4b628e6cbde84f5d34c8c0c3a81e2a2a336
xcngdscxys-eng <xcngdscxys@privaterelay.appleid.com>
2025-12-18 21:19:48 +0100

    Create intents.ts

---
+++ api/data/intents.ts
@@ -0,0 +1,22 @@
+export const intents = [
+  {
+    name: "choix_permis",
+    keywords: ["automatique", "manuel", "boite", "boîte", "choisir"]
+  },
+  {
+    name: "tarifs",
+    keywords: ["prix", "tarif", "forfait", "coût", "combien"]
+  },
+  {
+    name: "cpf",
+    keywords: ["cpf", "financement", "compte formation"]
+  },
+  {
+    name: "pieces",
+    keywords: ["documents", "pièces", "papier", "inscription"]
+  },
+  {
+    name: "contact",
+    keywords: ["contact", "rappel", "téléphone", "rdv"]
+  }
+];
